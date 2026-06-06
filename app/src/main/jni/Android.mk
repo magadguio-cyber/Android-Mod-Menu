68a2f03f876328fc23f14485fa9941dcdccf322c
magadguio-cyber <magadguio@gmail.com>
2026-06-06 18:13:02 +0300

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -10,8 +10,11 @@ LOCAL_SRC_FILES := Main.cpp \
                    imgui/imgui_widgets.cpp \
                    imgui/imgui_tables.cpp
 
+# Жорстко вказуємо компілятору, де шукати заголовочні файли imgui
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/imgui
+
+LOCAL_CFLAGS := -I$(LOCAL_PATH) -I$(LOCAL_PATH)/imgui
 
 LOCAL_LDLIBS := -llog -landroid
 
