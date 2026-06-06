aedf839851bc0c8f3a1a805e7e0c18f35668c426
magadguio-cyber <magadguio@gmail.com>
2026-06-06 18:36:38 +0300

    Rename module from MyModMenu to BlackRussiaMenu

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := MyModMenu
+LOCAL_MODULE    := BlackRussiaMenu
+
 LOCAL_SRC_FILES := Main.cpp \
                    imgui/imgui.cpp \
                    imgui/imgui_demo.cpp \
@@ -10,11 +11,10 @@ LOCAL_SRC_FILES := Main.cpp \
                    imgui/imgui_widgets.cpp \
                    imgui/imgui_tables.cpp
 
-# Жорстко вказуємо компілятору, де шукати заголовочні файли imgui
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/imgui
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH) -I$(LOCAL_PATH)/imgui
+LOCAL_CFLAGS := -I$(LOCAL_PATH) -I$(LOCAL_PATH)/imgui -O2
 
 LOCAL_LDLIBS := -llog -landroid
 
